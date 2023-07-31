b6e220363bf051702f54818c500161534d1bf1d6
Walid <walid.djebara3@gmail.com>
2023-07-31 11:51:38 +0200

    2nd commit

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const albumController = require('../controllers/albumController');
 
+router.get('/albums', albumController.albums);
+router.get('/albums/:id', albumController.album);
+router.post('/albums/:id', albumController.addImage);
+
 router.get('/albums/create', albumController.createAlbumForm);
 
 router.post('/albums/create', albumController.createAlbum);
